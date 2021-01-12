393a36b075a85aa7709953975a0e0ad339c441f5
nimowairimu <nimowairimu25@gmail.com>
2021-01-12 07:55:53 +0300

    Add changes made.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ function Pizza(flavor ,toppings,size, quantity, price) {
   this.toppings = toppings;
   this.size = size;
   this.quantity = quantity;
+  this.crust = crust;
   this.price = price;
 }
 
@@ -10,6 +11,7 @@ var small=500;
 var medium=750;
 var large= 950;
 var topping =100;
+var crust =50;
 
 
 $(document).ready(function() {
@@ -20,26 +22,27 @@ $("#order").click(
     flavor=$('#flavor').val();
     toppings=$('#toppings').val();
     size=$('#size').val();
+    crust=$('#crust').val();
     quantity=parseInt($('#quantity').val());
 
     // Getting the total amount
-    if (size == "small" && toppings == ""){
+    if (size == "small" && toppings == ""|| crust ==""){
       var price = small * quantity;
     }
-    else if (size == "small" && toppings == "Ham" || toppings == "Bacon"){
-      var price = small * quantity + topping;
-    }if(size == "medium" && toppings =="" ){
+    else if (size == "small" && toppings == "Ham" || toppings == "Bacon" && crust =="Thin" || crust =="Thick" || crust=="Stuffed"){
+      var price = small * quantity + topping + crust;
+    }if(size == "medium" && toppings =="" || crust =="" ){
       var price = medium * quantity;
-    }else if(size == "medium" && toppings == "Ham" || toppings == "Bacon"){
-      var price = medium * quantity + topping;
-    }if (size == "large" && toppings == ""){
+    }else if(size == "medium" && toppings == "Ham" || toppings == "Bacon" && crust =="Thin" || crust =="Thick" || crust=="Stuffed"){
+      var price = medium * quantity + topping+ crust;
+    }if (size == "large" && toppings == "" || crust ==""){
       var price = large * quantity;
-    }else if (size == "large" && toppings =="Ham" || toppings == "Bacon") {
-      var price= large * quantity + topping;
+    }else if (size == "large" && toppings =="Ham" || toppings == "Bacon" && crust =="Thin" || crust =="Thick" || crust=="Stuffed") {
+      var price= large * quantity + topping+ crust;
     }
 
     var newPizza = new Pizza(flavor,toppings,size,quantity,price);
-    var newRow = '<tr><th id="flav">' + newPizza.flavor + '</th><td id="tp">' + $("#toppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="sz">' + $("#size option:selected").text() + " - " + newPizza.size + '</td><td id="bxs">' + $("#quantity option:selected").text() + " - " + newPizza.quantity + '</td><td id="total">' + newPizza.price + '</td></tr>'
+    var newRow = '<tr><th id="flav">' + newPizza.flavor + '</th><td id="tp">' + $("#toppings option:selected").text() + " - " + newPizza.toppings + '</td><td id="sz">' + $("#size option:selected").text() + " - " + newPizza.size + '</td><td id="quantity">' + $("#quantity option:selected").text() + " - " + newPizza.quantity + '</td><td id="total">' + newPizza.price + '</td></tr>'
 $("#pizza").append(newRow);
 
 
